c08f981524f2c53edf6a72ba6e5e5c4da2d586b1
trieunhd <dongtrieuit1@gmail.com>
2021-11-05 10:34:58 +0700

    update Icoin Trieu

--- FarmSys/src/com/farmsys/ui/ChamSoc.java
+++ FarmSys/src/com/farmsys/ui/ChamSoc.java
@@ -52,6 +52,8 @@ public class ChamSoc extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jPanel1.setBackground(new java.awt.Color(0, 255, 0));
+
         lblTitle.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         lblTitle.setForeground(new java.awt.Color(0, 0, 204));
         lblTitle.setText("GIAO VIỆC CHĂM SÓC");
