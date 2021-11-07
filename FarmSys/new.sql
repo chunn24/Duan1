3e27a474f0260db8516166d744b0f25b1eb83ec2
chunn24 <trungvuong528@gmail.com>
2021-11-07 20:16:56 +0700

    new,qurrey

--- FarmSys/new.sql
+++ FarmSys/new.sql
@@ -21,16 +21,17 @@ Create table NhanVien (
 	VaiTro bit not null,
 	Hinh varchar(30) 
 );
-
 go
-Create table DanTrong (	
-	MaDan int IDENTITY(0,1) primary key not null,
-	TenDan nvarchar(30) not null,
+
+Create table GianTrong (	
+	MaGian int IDENTITY(0,1) primary key not null,
+	TenGian nvarchar(30) not null,
 	TrangThai bit,
 	/* "0: chưa trồng
 		1: trồng" */
 );
 go
+
 Create table LoaiCayTrong (	
 	MaCay int IDENTITY(0,1) primary key not null,
 	TenCay nvarchar(30)not null,
@@ -42,22 +43,36 @@ Create table LoaiCayTrong (
 	Hinh varchar(30) not null
 );
 go
+
 Create table TrongCay (	
-	MaDan int not null,
+	MaGian int not null,
 	MaCay int not null,
 	NguoiTao varchar(15),
 	NguoiTrong varchar(15),
 	NgayTrong date,
 	
-	primary key (MaDan,MaCay),
-	foreign key (MaDan) references DanTrong(MaDan),
+	primary key (MaGian,MaCay),
+	foreign key (MaGian) references GianTrong(MaGian),
 	foreign key (MaCay) references LoaiCayTrong(MaCay),
 	foreign key (NguoiTrong) references NhanVien(MaNV)
 );
 go
+
+create table CongViec(
+	TenCV int,
+	MoTa nvarchar(200),
+	NguoiGiaoViec varchar(15),
+	NguoiNhanViec varchar(15),
+	NgayBatDau date,
+	NgayKetThuc date,
+
+);
+go
+
 Create table NhatKy (	
 	MaCV int IDENTITY(0,1),
 	TenCV  int, /*   0.TRong cay, 1.Cham Soc, 2.Thu hoach  */
+	MaGian int ,
 	ChiTiet nvarchar(200),
 	NguoiTao varchar(15),
 	NhanVien varchar(15),
@@ -72,29 +87,34 @@ Create table NhatKy (
 
 	Primary key (MaCV),
 	foreign key (Nhanvien) references NhanVien(MaNV),
+	foreign key (MaGian) references GianTrong(MaGian)
 );
-
 go
-insert into NhanVien values 
+
+
+insert into NhanVien 
+values 
  ('Trung','240102',N'Vương Nguyên Trung',1,'trung@fpt.edu.vn',10000000,1,'a.png'),
- ('TrieuNHD','123456',N'Vương Nguyên Trung',1,'trieu@fpt.edu.vn',10000000,1,'b.png'),
- ('Tri','123456',N'Vương Nguyên Trung',1,'Tri@fpt.edu.vn',10000000,1,'c.png'),
- ('Dung','123456',N'Vương Nguyên Trung',1,'Dung@fpt.edu.vn',10000000,1,'d.png'),
- ('Hai','123456',N'Vương Nguyên Trung',1,'Hai@fpt.edu.vn',10000000,1,'e.png'),
- ('Tuan','123456',N'Vương Nguyên Trung',1,'Tuan@fpt.edu.vn',10000000,1,'f.png')
+ ('TrieuNHD','123456',N'Nguyễn Huỳnh Đông Triều',0,'trieu@fpt.edu.vn',10000000,0,'b.png'),
+ ('Tri','123456',N'Nguyễn Diệp Thế Tri',0,'Tri@fpt.edu.vn',10000000,0,'c.png'),
+ ('Dung','123456',N'Trần Tiến Dũng',0,'Dung@fpt.edu.vn',2000000,0,'d.png'),
+ ('Hai','123456',N'Giáp Văn Hải',0,'Hai@fpt.edu.vn',10000000,0,'e.png'),
+ ('Tuan','123456',N'Đinh Quang Tuấn',0,'Tuan@fpt.edu.vn',10000000,0,'f.png')
  go
- insert into DanTrong values 
-	 ('A1','0'),
-	 ('A2','0'),
-	 ('A3','0'),
-	 ('A4','0'),
-	 ('A5','0'),
-	 ('A6','0'),
-	 ('A7','0'),
-	 ('A8','0'),
+
+ insert into GianTrong (TenGian,TrangThai)
+ values 
+	 ('A1','1'),
+	 ('A2','1'),
+	 ('A3','1'),
+	 ('A4','1'),
+	 ('A5','1'),
+	 ('A6','1'),
+	 ('A7','1'),
+	 ('A8','1'),
 	 ('A9','0'),
 	 ('A10','0'),
-	  ('B1','0'),
+	 ('B1','0'),
 	 ('B2','0'),
 	 ('B3','0'),
 	 ('B4','0'),
@@ -104,7 +124,7 @@ insert into NhanVien values
 	 ('B8','0'),
 	 ('B9','0'),
 	 ('B10','0'),
-	  ('C1','0'),
+	 ('C1','0'),
 	 ('C2','0'),
 	 ('C3','0'),
 	 ('C4','0'),
@@ -115,6 +135,7 @@ insert into NhanVien values
 	 ('C9','0'),
 	 ('C10','0')
  go
+ 
  INSERT INTO LoaiCayTrong VALUES (N'Cây Chuối','30','1260','3.5','28','18','Hinh1.jpg');
 INSERT INTO LoaiCayTrong VALUES (N'Cây Việt Quốc','25','1300','2.7','30','12','Hinh2.jpg');
 INSERT INTO LoaiCayTrong VALUES (N'Cây Dưa Lưới','15','1000','7.0','27','13','Hinh3.jpg');
@@ -131,3 +152,20 @@ INSERT INTO LoaiCayTrong VALUES (N'Cây Bí Ngô','15','2570','6.6','33','09','H
 INSERT INTO LoaiCayTrong VALUES (N'Cây Khoai Tây','15','2180','6.5','30','11','Hinh14.jpg');
 INSERT INTO LoaiCayTrong VALUES (N'Cây Hành','15','1290','6.9','29','12','Hinh15.jpg');
 go
+
+insert into TrongCay (MaGian,MaCay,NguoiTao,NguoiTrong,NgayTrong)
+values 
+ ('1','1','Trung','TrieuNHD','2021-11-05'),
+ ('2','2','Trung','TrieuNHD','2021-11-04'),
+ ('3','4','Trung','Tri','2021-11-03'),
+ ('4','6','Trung','Tri','2021-11-02'),
+ ('5','5','Trung','Dung','2021-11-01'),
+ ('6','8','Trung','Dung','2021-11-01'),
+ ('7','4','Trung','Hai','2021-11-02'),
+ ('8','6','Trung','Hai','2021-11-03')
+ go
+
+ insert into NhatKy (TenCV,MaGian,ChiTiet,NguoiTao,NhanVien,NgayBatDau,NgayKetThuc,TrangThai)
+values 
+ ('0','1','Trồng cây giàn A1','Trung','TrieuNHD','2021-11-05','2021-11-06','')
+ go
