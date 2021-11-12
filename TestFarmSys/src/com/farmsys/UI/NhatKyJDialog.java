100c614fdf833d50927b1c1b466aac6bb474fad4
chunn24 <trungvuong528@gmail.com>
2021-11-12 13:41:30 +0700

    update testfarmsys

--- TestFarmSys/src/com/farmsys/UI/NhatKyJDialog.java
+++ TestFarmSys/src/com/farmsys/UI/NhatKyJDialog.java
@@ -7,10 +7,15 @@ package com.farmsys.UI;
 
 import com.farmsys.DTO.CongViec;
 import com.farmsys.DTO.NhatKy;
+import com.farmsys.Helper.MsgBox;
 import com.farmsys.dao.CongViecDAO;
 import com.farmsys.dao.NhatKyDAO;
+import java.awt.print.PrinterException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -89,6 +94,11 @@ public class NhatKyJDialog extends javax.swing.JFrame {
         btnXuatPDF.setText("Xuất PDF");
         btnXuatPDF.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         btnXuatPDF.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
+        btnXuatPDF.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnXuatPDFActionPerformed(evt);
+            }
+        });
 
         lblTrangThai.setFont(new java.awt.Font("Tahoma", 0, 13)); // NOI18N
         lblTrangThai.setForeground(new java.awt.Color(248, 241, 241));
@@ -265,6 +275,10 @@ public class NhatKyJDialog extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_cboLocTrangThaiActionPerformed
 
+    private void btnXuatPDFActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnXuatPDFActionPerformed
+        print();
+    }//GEN-LAST:event_btnXuatPDFActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -391,18 +405,36 @@ public class NhatKyJDialog extends javax.swing.JFrame {
 
     private String trangThai(NhatKy nk) {
         String status = null;
-        if (nk.getTrangThai() == 0) {
-            status = "Chưa nhận";
-        } else if (nk.getTrangThai() == 1) {
-            status = "Đang làm";
-        } else if (nk.getTrangThai() == 2) {
-            status = "Từ chối";
-        } else if (nk.getTrangThai() == 3) {
-            status = "Hoàn thành";
-        } else {
-            status = "Hoàn thành muộn";
+        switch (nk.getTrangThai()) {
+            case 0:
+                status = "Chưa nhận";
+                break;
+            case 1:
+                status = "Đang làm";
+                break;
+            case 2:
+                status = "Từ chối";
+                break;
+            case 3:
+                status = "Hoàn thành";
+                break;
+            default:
+                status = "Hoàn thành muộn";
+                break;
         }
         return status;
     }
 
+    void print() {
+        try {
+            Boolean print = tblNhatKy.print();
+            if (print) {
+                MsgBox.alert(this, "Đã xuất file PDF");
+            } else {
+                JOptionPane.showMessageDialog(this, "Error !", "Printer", JOptionPane.ERROR_MESSAGE);
+            }
+        } catch (PrinterException ex) {
+            Logger.getLogger(NhanVienJDialog.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
